b7fa9d9a5c9c0f1acd7777610f2d433c29302574
Aaron Shearer <119887592+aaron-shearer-hometown@users.noreply.github.com>
2023-03-03 12:19:18 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+const myString = 'my string',
+      myNumber = 1234567890,
+      myBoolean = true,
+      myArray = [
+        myString, 
+        myNumber, 
+        myBoolean
+      ],
+      myObject = { 
+        firstName: 'Aaron', 
+        lastName: 'Shearer', 
+        stuff: myArray 
+      };
